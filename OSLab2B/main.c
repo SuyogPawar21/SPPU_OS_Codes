775ed1c91ae3e3c66f020ffd4054ac0cf81a4cae
Suyog Pawar <suyogpawar3010@gmail.com>
2023-09-29 12:33:03 +0530

    main.c, child.c and output.txt added

--- OSLab2B/main.c
+++ OSLab2B/main.c
@@ -1,43 +1,68 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <malloc/_malloc.h>
 
-#define MAXNUMBERSCOUNT 100
+const int MAXSTRINGCOUNT = 10;
 
-void bubbleSort(int numbers[], int numbersCount) {
-    for (int i = 0; i < numbersCount; i++) {
-        for (int j = 0; j < numbersCount - i - 1; j++) {
-            if (numbers[j] > numbers[j + 1]) {
-                int temp = numbers[j];
-                numbers[j] = numbers[j + 1];
-                numbers[j + 1] = temp;
+void bubbleSort(char** strings, int stringCount) {
+
+    for (int i = 0; i < stringCount; i++) {
+        for (int j = 0; j < stringCount - i - 1; j++) {
+            if (strcmp(strings[j], strings[j + 1]) > 0) {
+                char* temp = strings[j];
+                strings[j] = strings[j + 1];
+                strings[j + 1] = temp;
             }
         }
+    }
+
+}
+
+void printStrings(char** strings, int stringCount) {
+    for (int i = 0; i < stringCount; i++) {
+        printf("%s ", strings[i]);
+    }
+}
+
+void freeMemory(char** strings, int stringCount) {
+    for (int i = 0; i < stringCount; i++) {
+        free(strings[i]);
     }
 }
 
 int main() {
 
-    int numbers[MAXNUMBERSCOUNT];
-    int numbersCount;
+    int stringCount, stringSize;
+    printf("Enter the number of strings: ");
+    scanf("%i", &stringCount);
 
-    printf("Enter the number of numbers: ");
-    scanf("%i", &numbersCount);
+    char** strings[MAXSTRINGCOUNT];
 
-    for (int i = 0; i < numbersCount; i++) {
-        printf("Enter number: ");
-        scanf("%i", &numbers[i]);
+    for (int i = 0; i < stringCount; i++) {
+
+        printf("Enter string size: ");
+        scanf("%i", &stringSize);
+
+        strings[i] = malloc(stringSize + 1);
+
+        printf("Enter string: ");
+        scanf("%s", strings[i]);
+
     }
 
-    bubbleSort(numbers, numbersCount);
+    bubbleSort(strings, stringCount);
 
     pid_t childPid = fork();
 
     if (childPid == 0) {
-        execv();
+        execve("OSLab2B/child.exe", strings, NULL);
     } else {
-        printf("Child's Process ID: %i", childPid);
         wait(NULL);
+        printf("Parent Process: ");
+        printStrings(strings, stringCount);
+        freeMemory(strings, stringCount);
     }
 
     return 0;
